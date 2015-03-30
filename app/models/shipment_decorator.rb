5b58ebdf1be5d876a5e41205c1f61c36f852cc51
al.bobrov <al.bobrov.mail@gmail.com>
2015-03-30 22:18:03 +0500

    rewrite admin shipment quantity view

---
+++ app/models/shipment_decorator.rb
@@ -0,0 +1,19 @@
+
+Spree::Shipment.class_eval do
+  def manifest
+    # Grouping by the ID means that we don't have to call out to the association accessor
+    # This makes the grouping by faster because it results in less SQL cache hits.
+    inventory_units.group_by(&:variant_id).map do |variant_id, units|
+      units.group_by(&:line_item_id).map do |line_item_id, units|
+
+        states = {}
+        units.group_by(&:state).each { |state, iu| states[state] = iu.first.line_item.quantity.to_f }
+
+        line_item = units.first.line_item
+        variant = units.first.variant
+        # binding.pry
+        ManifestItem.new(line_item, variant, units.first.line_item.quantity.to_f, states)
+      end
+    end.flatten
+  end
+end
--- app/overrides/replace_integer_quantity.rb
+++ app/overrides/replace_integer_quantity.rb
@@ -8,11 +8,11 @@ Deface::Override.new(:virtual_path => "spree/orders/_line_item",
                      :replace => '[data-hook="cart_item_quantity"]',
                      :partial => "/spree/orders/line_item_quantity",
                      :disabled => false)
-Deface::Override.new(:virtual_path => "spree/admin/orders/_shipment_manifest",
-                     :name => "replace_admin_item_quantity",
-                     :replace => '[class="item-qty-show align-center"]',
-                     :partial => "/spree/admin/orders/admin_item_quantity",
-                     :disabled => false)
+# Deface::Override.new(:virtual_path => "spree/admin/orders/_shipment_manifest",
+#                      :name => "replace_admin_item_quantity",
+#                      :replace => '[class="item-qty-show align-center"]',
+#                      :partial => "/spree/admin/orders/admin_item_quantity",
+#                      :disabled => false)
 Deface::Override.new(:virtual_path => "spree/admin/orders/_shipment_manifest",
                      :name => "replace_admin_edit_quantity",
                      :replace => '[class="item-qty-edit hidden"]',
